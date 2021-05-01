e56c14ab1709270e05b8365cdb14de65756245eb
MikePai <ye518612@gmail.com>
2021-05-01 17:50:04 +0800

    Initial Commit

---
+++ ConanSpell/ConanSpellApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConanSpellApp.swift
+//  ConanSpell
+//
+//  Created by 白謹瑜 on 2021/5/1.
+//
+
+import SwiftUI
+
+@main
+struct ConanSpellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConanSpell/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConanSpell
+//
+//  Created by 白謹瑜 on 2021/5/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
